96acbf5299ea792c3fbe8e0a947868f56fd4e214
Ole Begemann <ole@oleb.net>
2017-02-13 23:24:06 +0100

    Add Property.attributeString, .typeEncoding, and .attributes

--- Sources/Property.swift
+++ Sources/Property.swift
@@ -12,4 +12,38 @@ public final class Property {
     public var name: String {
         return String(cString: property_getName(base))
     }
+
+    public var attributeString: String {
+        return String(cString: property_getAttributes(base))
+    }
+
+    public var typeEncoding: String {
+        guard let typeEncoding = attributes()["T"] else {
+            fatalError("typeEncoding of property \(name) is unexpectedly nil")
+        }
+        return typeEncoding
+    }
+
+    /// - Complexity: O(_n_) where _n_ is the number of attributes.
+    /// - TODO: Memoize/cache the result to make properties that access
+    ///   the attributes O(1). This shouldn't be a problem because a
+    ///   property's attributes are immutable.
+    public func attributes() -> [String: String] {
+        var attributesCount: UInt32 = 0
+        let attributeList = property_copyAttributeList(base, &attributesCount)
+        guard let list = attributeList else { return [:] }
+        let listLength = Int(attributesCount)
+        defer {
+            list.deinitialize(count: listLength)
+            list.deallocate(capacity: listLength)
+        }
+        let buffer = UnsafeBufferPointer(start: list, count: listLength)
+        var result: [String: String] = [:]
+        for attribute in buffer {
+            let name = String(cString: attribute.name)
+            let value = String(cString: attribute.value)
+            result[name] = value
+        }
+        return result
+    }
 }
--- Tests/FuntimeTests/PropertyTests.swift
+++ Tests/FuntimeTests/PropertyTests.swift
@@ -9,5 +9,23 @@ class PropertyTests: XCTestCase {
         XCTAssertEqual(sut[0].name, "firstName")
         XCTAssertEqual(sut[1].name, "lastName")
     }
+
+    func testAttributeString() {
+        let cls = Class(base: NSObjectSubclass.self)
+        let sut = cls.properties()[0]
+        XCTAssertEqual(sut.attributeString, "T@\"NSString\",N,C,VfirstName")
+    }
+
+    func testTypeEncoding() {
+        let cls = Class(base: NSObjectSubclass.self)
+        let sut = cls.properties()[0]
+        XCTAssertEqual(sut.typeEncoding, "@\"NSString\"")
+    }
+
+    func testLifetime() {
+        var cls: Class? = Class(base: NSObjectSubclass.self)
+        let sut = cls?.properties()[0]
+        cls = nil
+        XCTAssertEqual(sut?.typeEncoding, "@\"NSString\"")
     }
 }
